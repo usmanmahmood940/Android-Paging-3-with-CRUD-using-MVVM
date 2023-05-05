fd3a58604489391b0b3f54af59b11c9d8b2df981
Usman <usman.mahmood@brainxtech.com>
2023-05-05 12:56:40 +0500

    Crud Implemented

--- app/src/main/java/com/example/paging_3_android/MainActivity.kt
+++ app/src/main/java/com/example/paging_3_android/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.paging_3_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.ExperimentalPagingApi
+import androidx.paging.filter
+import androidx.paging.map
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.paging_3_android.Paging.LoaderAdapter
 import com.example.paging_3_android.Paging.QuotePagingAdapter
 import com.example.paging_3_android.databinding.ActivityMainBinding
+import com.example.paging_3_android.models.Result
+import com.example.paging_3_android.models.SampleViewEvents
 import com.example.paging_3_android.viewModels.QuoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
+import java.util.Timer
 
 @ExperimentalPagingApi
 @AndroidEntryPoint
@@ -32,28 +38,32 @@ class MainActivity : AppCompatActivity() {
 
         quoteViewModel = ViewModelProvider(this).get(QuoteViewModel::class.java)
 
-        adapter = QuotePagingAdapter()
+        adapter = QuotePagingAdapter(quoteViewModel)
 
         mainBinding.rvQuoteList.layoutManager = LinearLayoutManager(this)
         mainBinding.rvQuoteList.setHasFixedSize(true)
+        mainBinding.rvQuoteList.adapter = adapter
+
         mainBinding.rvQuoteList.adapter = adapter.withLoadStateHeaderAndFooter(
             header = LoaderAdapter(),
             footer = LoaderAdapter()
         )
 
-//        // For flow
-//        lifecycleScope.launch {
-//            quoteViewModel.list.collectLatest { response->
-//                Log.d("main", "onCreate: $response")
-//                adapter.submitData(response)
-//            }
 //
-//        }
+
+        mainBinding.btnInsertTop.setOnClickListener {
+            quoteViewModel.onViewEvent(SampleViewEvents.InsertItemHeader)
+        }
 
         // For liveData
-        quoteViewModel.list.observe(this){
+        quoteViewModel.pagingDataViewStates.observe(this){
             adapter.submitData(lifecycle,it)
+//            val list = adapter.snapshot().items
+      
         }
+//        quoteViewModel.list.observe(this){
+//            adapter.submitData(lifecycle,it)
+//        }
 
     }
 }
--- app/src/main/java/com/example/paging_3_android/Paging/QuotePagingAdapter.kt
+++ app/src/main/java/com/example/paging_3_android/Paging/QuotePagingAdapter.kt
@@ -3,23 +3,37 @@ package com.example.paging_3_android.Paging
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.paging_3_android.R
 import com.example.paging_3_android.models.Result
+import com.example.paging_3_android.models.SampleViewEvents
+import com.example.paging_3_android.viewModels.QuoteViewModel
 
-class QuotePagingAdapter : PagingDataAdapter<Result, QuotePagingAdapter.QuoteViewHolder>(COMPARATOR) {
+@ExperimentalPagingApi
+class QuotePagingAdapter(private val quoteViewModel: QuoteViewModel) : PagingDataAdapter<Result, QuotePagingAdapter.QuoteViewHolder>(COMPARATOR) {
 
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val quote = itemView.findViewById<TextView>(R.id.tvQuote)
+        val btnEdit = itemView.findViewById<Button>(R.id.btnEdit)
+        val btnRemove = itemView.findViewById<Button>(R.id.btnRemove)
     }
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        val item = getItem(position)
+        val item = getItem(position) ?: return
         if (item != null){
             holder.quote.text = item.content
+        }
+        holder.btnEdit.setOnClickListener {
+            item.content = "Content"
+           quoteViewModel.onViewEvent(SampleViewEvents.Edit(item))
+        }
+        holder.btnRemove.setOnClickListener {
+            quoteViewModel.onViewEvent(SampleViewEvents.Remove(item))
         }
     }
 
